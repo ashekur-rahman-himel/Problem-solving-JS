db694f2907ff73a50dddc659fe2a678fb2e3e669
ashekur-rahman-himel <himalhossin20@gmail.com>
2025-04-15 11:15:33 +0600

    Starting

---
+++ 1st.js
@@ -0,0 +1,11 @@
+
+
+const numbers = [7, 10, 15, 8, 13, 18, 6];
+const evens = [];
+for (const num of numbers) {
+  if (num % 2 === 0) {
+    evens.push(num);
+  }
+}
+// [ 10, 8, 18, 6 ]
+console.log(" the even number is [" + evens + "]");
