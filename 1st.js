6317be31fdcfaf78237330cedcd627c8c2635e71
ashekur-rahman-himel <himalhossin20@gmail.com>
2025-04-16 09:24:06 +0600

    update

--- 1st.js
+++ 1st.js
@@ -1,15 +1,38 @@
 
+// // even number from array
 
-const numbers = [7, 10, 15, 8, 13, 18, 6];
-const evens = [];
-for (const num of numbers) {
-  if (num % 2 === 0) {
-    evens.push(num);
-  }
-}
-// [ 10, 8, 18, 6 ]
-console.log(" the even number is [" + evens + "]");
+// const numbers = [7, 10, 15, 8, 13, 18, 6];
+// const evens = [];
+// for (const num of numbers) {
+//   if (num % 2 === 0) {
+//     evens.push(num);
+//   }
+// }
 
-console.log(" the even number is [" + evens + "]");
+// console.log(" the even number is [" + evens + "]");
 
-console.log(" the even number is [" + evens + "]");
+// function isPrime(num) { 
+
+//   if (num <= 1) return false; 
+
+//   for (let i = 2; i <= Math.sqrt(num); i++) { 
+
+//     if (num % i === 0) return false; 
+
+//   } 
+
+//   return true; 
+
+// } 
+  //loop
+
+  // for(var i = 1; i<=10; i++){
+  //   console.log(i);
+
+  // }
+  // console.log(i);
+
+  // let i = 10;
+  // while(i--){
+  //   console.log(i);
+  // }
