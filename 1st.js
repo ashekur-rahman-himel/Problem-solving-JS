b1f19fae3912168c2ca8cd9a36ab4bd1ed47d9c8
ashekur-rahman-himel <himalhossin20@gmail.com>
2025-05-04 15:30:35 +0600

    update

--- 1st.js
+++ 1st.js
@@ -36,3 +36,189 @@
   // while(i--){
   //   console.log(i);
   // }
+  // javascript program that displays the largest integer among two integers
+// var num1 = 32;
+// var num2 = 32;
+
+// if(num1>num2){
+//   console.log("num1 is largest number");
+// }else if(num1 < num2){
+//   console.log("Num2 is a largest number");
+// }else{
+//   console.log("thay equal");
+  
+// }
+
+//Sort Numbers
+// var arr = [1,2,-1,0];
+
+// var sort = arr.sort().reverse();
+// console.log(sort);
+
+// var larg = [-5, -2, -6, 0, -1];
+
+
+
+// console.log(Math.max(...larg));
+
+
+/* 
+for(let i = 0; i <= 15; i++){
+  if(i % 2 ===0){
+    console.log(i +  "  even number");
+  }else{
+    console.log(i +  "  odd number");
+  }
+  
+} */
+
+
+/*   function addNum(a,b) {
+    return a*b;
+    
+  }
+  var a = addNum(40,5);
+  console.log(a); */
+
+/*   function user(name, age,loc){
+    return{
+      name:name,
+      age: age,
+      loc:loc
+    }
+  }
+  var person = user("Himel" , 26, "Dhaka");
+  console.log("your name " + person.name +" and your age " + person.age); */
+
+/*   function user(name, age, loc) {
+    return {
+      name: name,
+      age: age,
+      loc: loc
+    };
+  }
+  
+  const users = [];
+  
+  users.push(user("Himel", 26, "Dhaka"));
+  users.push(user("Sadia", 30, "Chittagong"));
+  
+  users.forEach(u => {
+    console.log(`নাম: ${u.name}, বয়স: ${u.age}, লোকেশন: ${u.loc}`);
+  }); */
+
+ /*  const person = {
+    name: "John",
+   age: 30,
+    city: "New York"
+   };
+  
+   Build a Text
+ let text = "";
+  for (let x in person) {
+    text += person[x] + " ";
+  };
+
+  console.log(text); */
+
+// call back function
+
+/* function greet(name, callback) {
+  console.log("Hi " + name);
+  callback();
+}
+
+function sayBye() {
+  console.log("Bye!");
+}
+
+function sayBye() {
+  console.log("Hi Abid!");
+}
+
+greet("Rahim", sayBye); */
+
+// Synchronous
+
+/* function greet(name){
+  console.log("Hello " +  name);
+  
+}
+
+function sayBey(){
+  console.log("Good Bye");
+  
+}
+
+greet("Himel");
+sayBey(); */
+
+
+/* Asynchronous 
+
+function greet(name, callback) {
+  console.log("Hello " + name);
+  setTimeout(callback, 2000); // 2 seconds after calling callback
+}
+
+function sayBye() {
+  console.log("Goodbye!");
+}
+
+greet("Rahim", sayBye); */
+
+
+/* function sample (a, b, cb) {
+  var c = a + b
+  var d = a - b
+  var result = cb(c,d)
+  return result
+}
+
+function sum(a, b) {
+  return a + b
+}
+
+var result = sample(12, 40, sum)
+console.log(result); */
+
+
+/* const fruits = ["apple", "Banana", "mango"];
+
+fruits.forEach((element, index) => {
+
+  console.log(index + ":" + element);
+  
+}); */
+
+
+/* const users = [
+  { name: "Alice", age: 20 },
+  { name: "Bob", age: 25 },
+  { name: "Charlie", age: 30 }
+];
+
+users.forEach(user => {
+  console.log(`${user.name} is ${user.age} years old`);
+  
+
+}) */
+
+  let myPromise = new Promise(function(resolve, reject) {
+    let success = true;
+    
+    if (success) {
+      resolve("Data loaded successfully!");
+    } else {
+      reject("Error loading data.");
+    }
+  });
+
+  myPromise
+  .then(function(result) {
+    console.log(result);  // যদি resolve হয়
+  })
+  .catch(function(error) {
+    console.log(error);   // যদি reject হয়
+  });
+ 
