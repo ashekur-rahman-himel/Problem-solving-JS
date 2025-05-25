028bcb076dc6d4f99467d6cedc099e40359ad9ab
ashekur-rahman-himel <himalhossin20@gmail.com>
2025-05-25 14:52:04 +0600

    update

--- practice.js
+++ practice.js
@@ -95,5 +95,15 @@ function getResult(value){
 }
 calculate(10,20,getResult) */
 
+function multiply(num1, num2) {
+if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+return 'Please provide a number';
+}
+const mult = num1 * num2;
+return mult;
+}
+
+const result = multiply(5, 7);
+console.log(result);
 
 
